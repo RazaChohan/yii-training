582e685728119e2ea01a5ea33f5d180c433b4830
Raza Chohan <muhammad.raza@coeus-solutions.de>
2015-07-07 15:31:20 +0500

    Desgin Changes
     Changes to be committed:
    	modified:   views/site/login.php
    	modified:   web/css/site.css

--- views/site/login.php
+++ views/site/login.php
@@ -23,7 +23,7 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]); ?>
 
-    <?= $form->field($model, 'username') ?>
+    <?= $form->field($model, 'username',[]) ?>
 
     <?= $form->field($model, 'password')->passwordInput() ?>
 
@@ -32,14 +32,14 @@ $this->params['breadcrumbs'][] = $this->title;
     ])->checkbox() ?>
 
     <div class="form-group">
-        <div class="col-lg-offset-1 col-lg-11">
+        <div class="col-lg-11">
             <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
         </div>
     </div>
 
     <?php ActiveForm::end(); ?>
 
-    <div class="col-lg-offset-1" style="color:#999;">
+    <div style="color:#999;">
         You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
         To modify the username/password, please check out the code <code>app\models\User::$users</code>.
     </div>
